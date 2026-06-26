e2f567db722abcd1472c24af1cf4083329cdabbf
Davi Lepore <davilepore218@gmail.com>
2026-06-26 17:35:14 -0300

    Delete menuqr directory

--- menuqr/src/app/dashboard/page.js
+++
@@ -1,7 +0,0 @@
-export default function Dashboard() {
-    return (
-      <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
-        <h1>Dashboard (em construção)</h1>
-      </div>
-    )
-  }
